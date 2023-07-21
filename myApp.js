85e83c21bc99a6345b2d04a595ebad3f13b74296
erinur <erinur@gmail.com>
2023-07-21 12:53:06 +0000

    cambios en myApp.js

--- myApp.js
+++ myApp.js
@@ -2,11 +2,15 @@ let express = require('express');
 let app = express();
 
 
+app.get('/', function(req, res) {
+  res.sendFile(absolutePath = __dirname + '/views/index.html');
+});
 
+app.use('/public', express.static(__dirname + '/public'));
 
-
-
-
+app.get('/json', function(req, res){
+  res.json({'message':'Hello json'})
+});
 
 
 
